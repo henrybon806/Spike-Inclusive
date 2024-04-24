690ac33609e7e115042dbf93b1c0940b92094723
Henry Bonomolo <hbono806@icloud.com>
2024-04-24 11:12:54 -0700

    Initial Commit

---
+++ Spike Inclusive/Spike_InclusiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spike_InclusiveApp.swift
+//  Spike Inclusive
+//
+//  Created by Henry Bonomolo on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Spike_InclusiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
